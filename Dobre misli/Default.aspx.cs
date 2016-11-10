106c32b5034fb8d82427514baa8d4f14cb032168
ŠTURM <gs9089@student.uni-lj.si>
2016-11-10 10:25:33 +0100

    fix

--- Dobre misli/Default.aspx.cs
+++ Dobre misli/Default.aspx.cs
@@ -11,12 +11,30 @@ namespace Dobre_misli
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(Application["misli"] == null)
+            {
+                List<Label> li2 = new List<Label>();
+                Application["misli"] = li2;
+            }
+
+            misli.Controls.Clear();
+            List<Label> li = (List<Label>)Application["misli"];
+            foreach(Label l in li)
+            {
+                misli.Controls.AddAt(0, l);
+
+            }
             
         }
 
         protected void dodajMisel_Click(object sender, EventArgs e)
         {
-                    
+            List<Label> li = (List<Label>)Application["misli"];
+            Label label = new Label();
+            label.Text = misel.Text + "<br ;";
+            li.Add(label);
+            Application["misli"] = li;
+            Server.TransferRequest(Request.Url.AbsolutePath, false);
         }
     }
     
